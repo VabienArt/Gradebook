e230b0507f9349378110a04f74f7a47b78a93040
Vabien <tristanfarrington@gmail.com>
2020-09-22 09:56:42 -0400

    added src

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book = new Book("Grade Book");
+            var grades = new List<double>(){73,86,93,45,89,91,79};
+            foreach (var number in grades){
+                book.addGrade(number);
+            }
+            var stats = book.GetStats();
+            Console.WriteLine($"Low: {stats.Low}");
+            Console.WriteLine($"High: {stats.High}");
+            Console.WriteLine($"Average: {stats.Average:N1}");
+        }
+    }
+}
